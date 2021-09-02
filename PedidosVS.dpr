b8cc51efe9573c7a79928dc9df7f3293efe358d2
Fabio Amorim <fabioamorim1501@gmail.com>
2021-09-02 10:24:03 -0300

    Projeto iniciado

---
+++ PedidosVS.dpr
@@ -0,0 +1,14 @@
+program PedidosVS;
+
+uses
+  Vcl.Forms,
+  untFormPrincipal in 'untFormPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
